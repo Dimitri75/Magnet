6af2194e30ce40ebc220d9797fa0bb114a4fe670
Kevin Suy <kevin.suy@me.com>
2015-10-24 15:19:16 -0400

    connection to server

--- app/src/main/java/kei/magnet/MainActivity.java
+++ app/src/main/java/kei/magnet/MainActivity.java
@@ -1,11 +1,31 @@
 package kei.magnet;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private static String serverURL = "http://localhost:8080/api";
+    private static String serverURL = "http://192.168.42.170:8080/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,4 +54,96 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void checkLogin(View V){
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    InputStream is = null;
+                    HttpClient httpclient = new DefaultHttpClient();
+                    HttpGet httppost = new HttpGet(serverURL);
+                    //httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                    HttpResponse response = httpclient.execute(httppost);
+                    is = response.getEntity().getContent();
+                    System.out.println(convertStreamToString(is));
+
+
+//                    String url = "http://my-json-feed";
+//
+//                    JSONObject jsonobject = new JSONObject();
+
+
+                }catch(Exception e){
+                    System.out.println("Fail");
+                }
+            }
+        };
+        thread.start();
+    }
+
+    private static String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
