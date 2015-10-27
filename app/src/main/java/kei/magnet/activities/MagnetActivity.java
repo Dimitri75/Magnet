7ab30f0cffa2e3849251d0f5de70b63669e078ba
Dimitri75 <bdimitri75@gmail.com>
2015-10-27 02:48:38 -0400

    Cleaning + suppression padding sur MagnetActivity

--- app/src/main/java/kei/magnet/activities/MagnetActivity.java
+++ app/src/main/java/kei/magnet/activities/MagnetActivity.java
@@ -13,23 +13,6 @@ import kei.magnet.R;
 
 public class MagnetActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_magnet);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//    }
-
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
 
