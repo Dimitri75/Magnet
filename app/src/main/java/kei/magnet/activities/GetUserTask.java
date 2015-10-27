ecb111bd6defa2e15b4c5af27ea6649da7f69c07
Dimitri75 <bdimitri75@gmail.com>
2015-10-26 23:15:24 -0400

    Merge modifications kévin

---
+++ app/src/main/java/kei/magnet/activities/GetUserTask.java
@@ -0,0 +1,60 @@
+package kei.magnet.activities;
+
+import android.os.AsyncTask;
+import android.support.v4.media.MediaBrowserServiceCompat;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Suiken on 26/10/2015.
+ */
+public class GetUserTask extends AsyncTask<URL, Void, JSONObject>{
+
+    protected JSONObject doInBackground(URL... url) {
+        JSONObject jsonObject = null;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) url[0].openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+
+            InputStream stream = connection.getInputStream();
+
+            jsonObject = new JSONObject(convertStreamToString(stream));
+
+        }catch(Exception e){
+            System.out.println("Connection to " + url[0].toString() + " failed");
+        }
+        return jsonObject;
+    }
+
+    private String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+}
