b087b25d6f11fc661e078c721f0180c8d4c81264
Moumoush <mehdi.bouchagour@laposte.net>
2015-11-20 21:34:07 -0500

    Merge branch 'master' of https://github.com/Dimitri75/Magnet
    Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/res/layout/content_sign_in.xml

--- app/src/main/java/kei/magnet/classes/ApplicationUser.java
+++ app/src/main/java/kei/magnet/classes/ApplicationUser.java
@@ -19,9 +19,7 @@ import java.util.List;
 public class ApplicationUser extends User{
     private List<Group> groups;
 
-    public List<Group> getGroups() {
-        return groups;
-    }
+
 
     public void setGroups(List<Group> groups) {
         this.groups = groups;
