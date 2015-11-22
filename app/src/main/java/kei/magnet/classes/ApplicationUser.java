3279c950eab4b66afec5acd6cf167364458a377d
Moumoush <mehdi.bouchagour@laposte.net>
2015-11-21 22:59:51 -0500

    Revert "Merge branch 'master' of https://github.com/Dimitri75/Magnet"
    This reverts commit b087b25d6f11fc661e078c721f0180c8d4c81264.

--- app/src/main/java/kei/magnet/classes/ApplicationUser.java
+++ app/src/main/java/kei/magnet/classes/ApplicationUser.java
@@ -19,7 +19,9 @@ import java.util.List;
 public class ApplicationUser extends User{
     private List<Group> groups;
 
-
+    public List<Group> getGroups() {
+        return groups;
+    }
 
     public void setGroups(List<Group> groups) {
         this.groups = groups;
