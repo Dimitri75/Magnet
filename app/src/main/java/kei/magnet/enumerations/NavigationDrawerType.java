312a3f43789354ee521088b7323156d7517b9b79
Kevin Suy <kevin.suy@me.com>
2015-11-24 12:04:44 -0500

    show groups and users

---
+++ app/src/main/java/kei/magnet/enumerations/NavigationDrawerType.java
@@ -0,0 +1,10 @@
+package kei.magnet.enumerations;
+
+/**
+ * Created by Suiken on 24/11/2015.
+ */
+public enum NavigationDrawerType {
+    TITLE,
+    GROUP,
+    USER
+}
--- app/src/main/java/kei/magnet/fragments/CustomDrawerAdapter.java
+++ app/src/main/java/kei/magnet/fragments/CustomDrawerAdapter.java
@@ -6,16 +6,25 @@ package kei.magnet.fragments;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import kei.magnet.R;
+import kei.magnet.enumerations.NavigationDrawerType;
 
 public class CustomDrawerAdapter extends ArrayAdapter<DrawerItem> {
 
@@ -44,9 +53,16 @@ public class CustomDrawerAdapter extends ArrayAdapter<DrawerItem> {
             drawerHolder = new DrawerItemHolder();
 
             view = inflater.inflate(layoutResID, parent, false);
-            drawerHolder.ItemName = (TextView) view
-                    .findViewById(R.id.drawer_itemName);
-            drawerHolder.icon = (ImageView) view.findViewById(R.id.drawer_icon);
+
+            drawerHolder.userTitle = (TextView) view.findViewById(R.id.userTitle);
+            drawerHolder.groupTitle = (TextView) view.findViewById(R.id.groupTitle);
+            drawerHolder.titleTitle = (TextView) view.findViewById(R.id.titleTitle);
+
+//            drawerHolder.icon = (ImageView) view.findViewById(R.id.drawer_icon);
+
+            drawerHolder.groupLayout = (LinearLayout) view.findViewById(R.id.groupLayout);
+            drawerHolder.userLayout = (LinearLayout) view.findViewById(R.id.userLayout);
+            drawerHolder.titleLayout = (LinearLayout) view.findViewById(R.id.titleLayout);
 
             view.setTag(drawerHolder);
 
@@ -55,17 +71,43 @@ public class CustomDrawerAdapter extends ArrayAdapter<DrawerItem> {
 
         }
 
-        DrawerItem dItem = (DrawerItem) this.drawerItemList.get(position);
+        DrawerItem dItem = this.drawerItemList.get(position);
 
-        drawerHolder.icon.setImageDrawable(view.getResources().getDrawable(
-                dItem.getImgResID()));
-        drawerHolder.ItemName.setText(dItem.getItemName());
+
+        if (dItem.getType() == NavigationDrawerType.TITLE){
+            //TODO
+            //            drawerHolder.icon.setImageDrawable(view.getResources().getDrawable(
+//                    dItem.getImgResID()));
+        } else if (dItem.getType() == NavigationDrawerType.USER) {
+            System.out.println("user");
+            drawerHolder.userLayout.setVisibility(LinearLayout.VISIBLE);
+            //drawerHolder.userLayout.setBackgroundColor(Color.GREEN);
+
+            drawerHolder.groupLayout.setVisibility(LinearLayout.INVISIBLE);
+
+            drawerHolder.titleLayout.setVisibility(LinearLayout.INVISIBLE);
+
+            drawerHolder.userTitle.setText(dItem.getTitle());
+            drawerHolder.userTitle.setVisibility(View.VISIBLE);
+        } else if(dItem.getType() == NavigationDrawerType.GROUP){
+            System.out.println("group");
+            drawerHolder.groupLayout.setVisibility(LinearLayout.VISIBLE);
+            //drawerHolder.groupLayout.setBackgroundColor(Color.BLUE);
+
+            drawerHolder.titleLayout.setVisibility(LinearLayout.INVISIBLE);
+
+            drawerHolder.userLayout.setVisibility(LinearLayout.INVISIBLE);
+
+            drawerHolder.groupTitle.setText(dItem.getTitle());
+            drawerHolder.groupTitle.setVisibility(View.VISIBLE);
+        }
 
         return view;
     }
 
     private static class DrawerItemHolder {
-        TextView ItemName;
-        ImageView icon;
+        TextView userTitle, groupTitle, titleTitle;
+        ImageView profilePicture;
+        LinearLayout titleLayout, userLayout, groupLayout;
     }
 }
