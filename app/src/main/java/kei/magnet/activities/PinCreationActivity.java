2259277131827825930fe7427e6392300d9e1755
SquallB <mazento2002@yaho.fr>
2015-11-28 21:52:07 -0500

    changements sur les dates picker pour la création de pin : utilisation de dialog

--- app/src/main/java/kei/magnet/activities/PinCreationActivity.java
+++ app/src/main/java/kei/magnet/activities/PinCreationActivity.java
@@ -1,6 +1,10 @@
 package kei.magnet.activities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -8,10 +12,12 @@ import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.json.JSONObject;
 
 import java.util.AbstractMap;
+import java.util.Calendar;
 
 import kei.magnet.R;
 import kei.magnet.classes.ApplicationUser;
@@ -19,15 +25,42 @@ import kei.magnet.classes.Group;
 import kei.magnet.classes.Location;
 import kei.magnet.task.CreatePinTask;
 
-public class PinCreationActivity extends AppCompatActivity {
+public class PinCreationActivity extends FragmentActivity {
     private ApplicationUser applicationUser;
     private EditText txtName;
     private EditText txtDescription;
     private Spinner spinnerGroups;
-    private DatePicker activationDate;
-    private DatePicker expirationDate;
     private Location location;
+    private EditText activationDate;
+    private EditText expirationDate;
 
+    int activationYear;
+    int activationMonth;
+    int activationDay;
+
+    int expirationYear;
+    int expirationMonth;
+    int expirationDay;
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +74,64 @@ public class PinCreationActivity extends AppCompatActivity {
 
         txtName = (EditText) findViewById(R.id.pin_creation_editText_PIN_NAME);
         txtDescription = (EditText) findViewById(R.id.pin_creation_editText_DESCRIPTION);
-        activationDate = (DatePicker) findViewById(R.id.pin_creation_datePicker_ACTIVATION);
-        expirationDate = (DatePicker) findViewById(R.id.pin_creation_datePicker_EXPIRATION);
         spinnerGroups = (Spinner) findViewById(R.id.pin_creation_spinner_GROUP);
+        activationDate = (EditText) findViewById(R.id.activationDateText);
+        expirationDate = (EditText) findViewById(R.id.expirationDateText);
 
         final ArrayAdapter<Group> myAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
                 android.R.id.text1,
                 applicationUser.getGroups());
         spinnerGroups.setAdapter(myAdapter);
+
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        expirationYear  = c.get(Calendar.YEAR);
+        expirationMonth = c.get(Calendar.MONTH);
+        expirationDay = c.get(Calendar.DAY_OF_MONTH);
+        expirationDate.setText(dateToString(expirationYear, expirationMonth, expirationDay));
+        activationYear  = expirationYear;
+        activationMonth = expirationMonth;
+        activationDay = expirationDay;
+        activationDate.setText(dateToString(activationYear, activationMonth, activationDay));
+    }
+
+    public void oncClick_showActivationDate(View v) {
+        DialogFragment newFragment = new DatePickerFragment() {
+            @Override
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+                // Create a new instance of DatePickerDialog and return it
+                return new DatePickerDialog(getActivity(), this, activationYear, activationMonth, activationDay);
+            }
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                activationYear = year;
+                activationMonth = month;
+                activationDay = day;
+                activationDate.setText(dateToString(year, month, day));
+            }
+        };
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+    public void oncClick_showExpirationDate(View v) {
+        DialogFragment newFragment = new DatePickerFragment() {
+            @Override
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+                // Create a new instance of DatePickerDialog and return it
+                return new DatePickerDialog(getActivity(), this, expirationYear, expirationMonth, expirationDay);
+            }
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                expirationYear = year;
+                expirationMonth = month;
+                expirationDay = day;
+                expirationDate.setText(dateToString(year, month, day));
+            }
+        };
+        newFragment.show(getFragmentManager(), "datePicker");
     }
 
     public void onClick_submit(View V) {
@@ -63,8 +145,8 @@ public class PinCreationActivity extends AppCompatActivity {
                     new AbstractMap.SimpleEntry<>("name", txtName.getText().toString()),
                     new AbstractMap.SimpleEntry<>("description", txtDescription.getText().toString()),
                     new AbstractMap.SimpleEntry<>("location", locationJSON.toString()),
-                    new AbstractMap.SimpleEntry<>("creation_time", datePickerToString(activationDate)),
-                    new AbstractMap.SimpleEntry<>("deletion_time", datePickerToString(expirationDate)),
+                    new AbstractMap.SimpleEntry<>("creation_time", dateToString(activationYear, activationMonth, activationDay)),
+                    new AbstractMap.SimpleEntry<>("deletion_time", dateToString(expirationYear, expirationMonth, expirationDay)),
                     new AbstractMap.SimpleEntry<>("group_id", String.valueOf(((Group) spinnerGroups.getSelectedItem()).getId()))
             );
         } catch (Exception e) {
@@ -76,12 +158,20 @@ public class PinCreationActivity extends AppCompatActivity {
         return applicationUser;
     }
 
-    //2015-11-24 07:18:07
-    private String datePickerToString(DatePicker picker) {
-        StringBuilder sb = new StringBuilder(picker.getYear());
-        sb.append("-").append(picker.getMonth()).append("-").append(activationDate.getDayOfMonth());
+    private String dateToString(int year, int month, int day) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(year).append("-");
+
+        if((month + 1) < 10) {
+            sb.append("0");
+        }
+        sb.append(month + 1).append("-");
+
+        if(day < 10) {
+            sb.append("-");
+        }
+        sb.append(day);
 
         return sb.toString();
     }
-
 }
